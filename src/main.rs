7711c112ad2d960fc452366d70cf5e20595a27cc
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-03-26 15:27:16 +0200

    refactor: add an example

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use adiscord::Client;
+use dotenv_codegen::dotenv;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new("10", dotenv!("TOKEN"), adiscord::TokenType::Bot);
+    match client.guilds.get("1089521338286342195").await {
+        Ok(guild) => println!("{:?}", guild.name),
+        Err(error) => println!("{:?}", error),
+    };
+
+    match client.guilds.get_preview("1089521338286342195").await {
+        Ok(guild) => println!("{:?}", guild),
+        Err(error) => println!("{:?}", error),
+    };
+}
