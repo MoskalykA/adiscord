fd7480f62917244d84eed6abd255ebe2c19397cb
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-03-29 18:25:40 +0200

    first commit

--- src/types/team/member/state.rs
+++
@@ -1,9 +0,0 @@
-use serde_repr::Deserialize_repr;
-
-#[allow(non_camel_case_types)]
-#[derive(Deserialize_repr, PartialEq, Debug)]
-#[repr(u8)]
-pub enum MemberState {
-    INVITED = 1,
-    ACCEPTED,
-}
